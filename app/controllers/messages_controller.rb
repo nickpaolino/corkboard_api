6263da7bf1c2b98bfb75061eea2a878ef044f65f
Nick Paolino <github email address>
2017-12-13 15:47:50 -0500

    Added create action to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,10 @@
 class MessagesController < ApplicationController
+  def create
+    user = User.find(1)
+    message = current_user.messages.build(message_params)
+    if message.save
+      ActionCable.server.broadcast 'room_channel',
+                                   content:  message.content,
+                                   username: message.user.username
+    end
 end
