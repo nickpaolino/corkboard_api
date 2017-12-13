ad4f4c40dce0114f98a146ae401a3d86e1042ac2
Nick Paolino <github email address>
2017-12-13 16:05:14 -0500

    Setup channel through Action Cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,24 @@
 class MessagesController < ApplicationController
+  def index
+  end
+
   def create
-    user = User.find(1)
+    current_user = User.find(1)
+
+    # Create a message entry in Active Record
     message = current_user.messages.build(message_params)
+
+    # If the message saves, broadcast to the room channel
     if message.save
       ActionCable.server.broadcast 'room_channel',
                                    content:  message.content,
                                    username: message.user.username
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
