8fd02fffd94863dd2ccbcc0c366ccf11e3ac56f6
Nick Paolino <github email address>
2017-12-13 16:46:49 -0500

    Added JWT gem and got the basic authentication boilerplate setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authorized
+
+  def issue_token(payload)
+    JWT.encode(payload, "supersecretcode")
+  end
+
+  def jwt_token
+    request.headers['Authorization']
+  end
+
+  def current_user
+    begin
+      decoded_token = JWT.decode(jwt_token, "supersecretcode")
+
+    rescue JWT::DecodeError
+      return nil
+    end
+
+    if decoded_token[0]["user_id"]
+      @current_user ||= User.find(decoded_token[0]["user_id"])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: {message: "Not welcome" }, status: 401 unless logged_in?
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+      resources :users
+
+      post "/auth", to: "sessions#create"
+    end
+  end
+  # get "/current_user", to: "sessions#show"
 
   mount ActionCable.server => '/cable'
 end
