36f06ecbb693308ee3aa27aba157eb2da31f6971
Nick Paolino <github email address>
2017-12-15 10:06:56 -0500

    Added a create action for the Users Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    render json: @user.to_json
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
