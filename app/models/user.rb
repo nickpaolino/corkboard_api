b5e8b948a89e735c80f9575b2394049178a15654
Nick Paolino <github email address>
2017-12-31 15:44:06 -0500

    Changed sessions response to send back the randomly generated position of the board for the user home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,9 @@ class User < ApplicationRecord
         id: board_user.board.id,
         subject: board_user.board.subject,
         public: board_user.board.public,
-        is_admin: board_user.is_admin
+        is_admin: board_user.is_admin,
+        left_position: board_user.left_position,
+        top_position: board_user.top_position
       }
 
       board_array << board_object
