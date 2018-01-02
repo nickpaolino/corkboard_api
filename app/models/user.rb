a55d1a0cf49954017d66cff030cfe7571be446de
Nick Paolino <github email address>
2018-01-02 16:01:35 -0500

    Removed changes made to the user helper method because they interfered with the rendering of the boards

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,18 @@ class User < ApplicationRecord
   def formatted_boards
     board_array = []
     self.board_users.each do |board_user|
-      if board_user.board
-        board_object = {
-          id: board_user.board.id,
-          subject: board_user.board.subject,
-          public: board_user.board.public,
-          is_admin: board_user.is_admin,
-          left_position: board_user.left_position,
-          top_position: board_user.top_position
-        }
-      end
+      board_object = {
+        id: board_user.board.id,
+        subject: board_user.board.subject,
+        public: board_user.board.public,
+        is_admin: board_user.is_admin,
+        left_position: board_user.left_position,
+        top_position: board_user.top_position
+      }
 
       board_array << board_object
     end
 
-    return board_array.compact
+    return board_array
   end
 end
